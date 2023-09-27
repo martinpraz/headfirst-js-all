d242b20a31854b149f21b62b23c9d80b72b85e85
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-09-26 20:21:38 -0400

    Add getMoreCostEffectiveSolution function

--- bubbles-array/bubbles.js
+++ bubbles-array/bubbles.js
@@ -3,6 +3,13 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52,
               69, 64, 66, 55, 52, 61, 46, 31, 57, 
               52, 44, 18, 41, 53, 55, 61, 51, 44];
 
+var costs = [.25, .27, .25, .25, .25, .25,
+             .33, .31, .25, .29, .27, .22,
+             .31, .25, .25, .33, .21, .25,
+             .25, .25, .28, .25, .24, .22,
+             .20, .25, .30, .25, .24, .25,
+             .25, .25, .27, .25, .26, .29];
+
 // function bubblePrinter(array) {
 //     let highestScore = 0;
 //     let bestSolutionList = [];
@@ -54,11 +61,27 @@ function getBestResults(array, highestScore) {
     return bestSolutionList;
 }
 
+function getMostCostEffectiveSolution(costs, scores, highestScore) {
+    let cost = 1;
+    let indexOfLowCost;
+
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] === highestScore && costs[i] < cost) {
+            indexOfLowCost = i;
+            cost = costs[i];
+        }
+    }
+
+    return indexOfLowCost;
+}
+
 printAndGetHighScore(scores);
 
 const highestScore = printAndGetHighScore(scores);
 const bestSolutionList = getBestResults(scores, highestScore);
+const mostCostEffective = getMostCostEffectiveSolution(costs, scores, highestScore);
 
-console.log(`Bubble tests: ${scores.length}`);
-console.log(`Highest bubble score: ${highestScore}`);
-console.log(`Solutions with the highest score: ${bestSolutionList}`);
+// console.log(`Bubble tests: ${scores.length}`);
+// console.log(`Highest bubble score: ${highestScore}`);
+// console.log(`Solutions with the highest score: ${bestSolutionList}`);
+console.log(mostCostEffective);
