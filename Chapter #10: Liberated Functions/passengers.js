c8ed5b9db3ee051286485824432e0d32d89f72ad
codentouge <142188340+newfordmartin@users.noreply.github.com>
2025-02-26 22:40:45 -0500

    feat: add function to process passengers with a test function

--- Chapter #10: Liberated Functions/passengers.js
+++ Chapter #10: Liberated Functions/passengers.js
@@ -1,8 +1,23 @@
 
+let passengers = [{name: "Jane Doloop", paid: true},
+    {name: "Dr. Evel", paid: true},
+    {name: "Sue Property", paid: false},
+    {name: "John Funcall", paid: true }];
+
+
 function checkNoFlyList(passenger) {
     return passenger.name = "Dr Evel";
 }
 
 function checkNotPaid(passenger) {
     return passenger.paid;
+}
+
+function processPassengers(passengers, testFunction) {
+    for (let i = 0; i < passengers.length; i++) {
+        if (testFunction(passengers[i])) {
+            return false;
+        }
+    }
+    return true;
 }
